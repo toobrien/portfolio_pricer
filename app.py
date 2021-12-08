a727f347c600ecca50cb4344419743a9f3292a29
Taylor O'Brien <toobrien@amazon.com>
2021-12-07 22:09:53 -0800

    underlying strings

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from dash import Dash
+from dash_html_components import P
 from dash.dependencies import Input, Output, State
 from ib.ib import ib
 from json import loads
@@ -6,29 +7,94 @@ from parsers import parse_ul_def
 from view import view
 
 
+# GLOBALS
+
+
 app = Dash(__name__)
 app.layout = view().get_layout()
+MAX_EXPIRIES = 5
 
+
+# FUNCTIONS
 
 '''
 @app.callback(
-    Output("underlyings_cell"),
-    Input("underlyings_submit"),
-    State("underlyings_text")
+    Output("underlyings_data", "value"),
+    Input("underlyings_control_submit", "n_clicks"),
+    State("underlyings_control_text", "value")
 )
 '''
-def get_underlyings(_, txt):
+def set_underlyings_data(_, txt: str) -> P:
 
     ul_defs = txt.split("\n")
+    uls = []
     res = []
+
+    # get underlyings
 
     for ul_def in ul_defs:
 
         args = parse_ul_def(ul_def, config)
-        uls = ib_client.underlying(**args)
-        res.extend(uls)
+        uls.extend(ib_client.underlying(**args))
 
-    return res
+    # prepare one line for each underlying:
+    #  - symbol
+    #  - price
+    #  - expirations
+    #  - strikes
+    #  - trading classes
+
+    for ul in uls:
+
+        contract = ul.get_contract()
+        option_chains = ul.get_option_chains()
+        
+        symbol = contract.localSymbol
+        price = ul.get_price()
+        expiries = []
+        strikes = []
+        trading_classes = []
+
+        for option_chain in option_chains:
+
+            trading_classes.append(option_chain.get_trading_class())
+
+        # assume expiries valid for all classes
+
+        expiries.extend(option_chains[0].get_expiries()[:MAX_EXPIRIES])
+
+        # take first, last, and middle strikes (or "-1" if ul price not available)
+
+        strikes.append(option_chains[0].get_strikes()[0])
+        if      price: strikes.extend(option_chains[0].get_atm_strikes(1, price))
+        else:   strikes.extend([-1, -1])
+        strikes.append(option_chains[0].get_strikes()[-1])
+        
+        strikes = [ str(strike) for strike in strikes ]
+
+        # format strings
+
+        symbol_string = f"{symbol:<6}"
+        price_string = f"{price:<6}"
+        expiries_string = f"[ {' '.join(expiries):<10} ]"
+        strikes_string = f"[ {strikes[0]:<6} ... {' '.join(strikes[1:3]):8} ... {strikes[3]:<6} ]"
+        trading_classes_string = f"[ {' '.join(trading_classes):<4} ]"
+
+        res.append(
+            symbol_string + 
+            price_string +
+            expiries_string +
+            strikes_string +
+            trading_classes_string
+        )
+
+    return P(
+        id = "underlyings_data_text",
+        children = [ "\n".join(res) ]
+    )
+
+
+# MAIN
 
 
 if __name__ == "__main__":
@@ -45,21 +111,17 @@ if __name__ == "__main__":
 
         ib_client.set_mkt_data_type(4)
 
-        uls = get_underlyings(
+        # TEST
+        set_underlyings_data(
             None,
             "\n".join(
                 [
                     "TSLA",
-                    "NGF2",
-                    "NG:3"
+                    "NG:5"
                 ]
             )
         )
 
-        for underlying in uls:
-
-            print(underlying)
-        
         app.run_server(
             host = config["dash_host"],
             port = config["dash_port"], 
--- view.py
+++ view.py
@@ -9,57 +9,98 @@ class view():
         ROWS = 10
         COLS = 30
 
-        control_cell = Td(
-            id = "control_cell",
+        # controls
+
+        underlyings_control_cell = Td(
+            id = "underlyings_control_cell",
             children = [
                 Textarea(
-                    id = "underlyings_text",
+                    id = "underlyings_control_text",
                     rows = ROWS,
                     cols = COLS
                 ),
                 Button(
-                    id = "underlyings_submit"
-                ),
+                    id = "underlyings_control_submit",
+                    children = "submit"
+                )
+            ]
+        )
+
+        legs_control_cell = Td(
+            id = "legs_control_cell",
+            children = [
                 Textarea(
                     id = "legs_text",
                     rows = ROWS,
                     cols = COLS
                 ),
                 Button(
-                    id = "legs_submit"
-                ),
+                    id = "legs_submit",
+                    children = "submit"
+                )
+            ]
+        )
+        
+        variables_control_cell = Td(
+            id = "variables_control_cell",
+            children = [
                 Textarea(
                     id = "variables_text",
                     rows = ROWS,
                     cols = COLS
                 ),
                 Button(
-                    id = "variables_submit"
+                    id = "variables_submit",
+                    children = "submit"
                 )
             ]
         )
 
-        underlyings_cell = Td(
-            id = "underlyings_cell"
+        controls_row = Tr(
+            id = "controls_row",
+            children = [ 
+                underlyings_control_cell,
+                legs_control_cell,
+                variables_control_cell
+            ]
         )
 
-        chart_cell = Td(
+        # underlyings_data_cell
+
+        underlyings_data_cell = Td(
+            id = "underlyings_data_cell",
+            children = [
+                Div(
+                    id = "underlyings_data",
+                    children = []
+                )
+            ]
+        )
+
+        underlyings_data_row = Tr(
+            id = "underlyings_data_row",
+            children = [ underlyings_data_cell ]
+        )
+
+        # payoffs
+
+        payoff_cell = Td(
             id = "chart_cell"
         )
 
-        main_row = Tr(
-            id = "main_row",
-            children = [
-                control_cell,
-                underlyings_cell,
-                chart_cell
-            ]
-        )
-        
-        self.layout = Table(
-            id = "app_view",
-            children = [ main_row ]
+        payoff_row = Tr(
+            id = "payoff_row",
+            children = [ payoff_cell ]
         )
 
-    
+        self.layout = Table(
+            id = "app_view",
+            children = [
+                payoff_row,
+                underlyings_data_row,
+                controls_row 
+            ]
+        )
+
+
     def get_layout(self): return self.layout
